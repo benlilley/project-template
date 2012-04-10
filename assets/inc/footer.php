c7719abb52995b1fd10d04585434349b36ecfb1a
Ben Lilley <benlilley@me.com>
2012-04-10 14:09:49 +1200

    Cleaned Javascript + Minor Tweaks
    - Removed some old javascript.
    - Updated jQuery to 1.7.2
    - Moved to lower case commenting.

--- assets/inc/footer.php
+++ assets/inc/footer.php
@@ -1,5 +1,5 @@
 	<!-- more javascript (in footer for speed) -->
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
 	<script src="/assets/js/custom.js"></script> 
 	
 	<!-- asynchronous google analytics -->
