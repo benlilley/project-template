d955a34eef99f66db43ba6f0852a3a8ba7771922
Ben Lilley <benlilley@me.com>
2012-08-03 15:14:53 +1200

    Switched from LESS framework to SASS for extra functionality and Compass support. Other minor tweaks.

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -3,17 +3,17 @@
 <!--[if IE 8 ]> <html class="ie8"> <![endif]-->
 <!--[if IE 9 ]> <html class="ie9"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <html> <!--<![endif]-->
-<head> 
-	<title>Project Title</title> 
-	
+<head>
+	<title>Project Title</title>
+
 	<!-- meta -->
 	<meta charset="utf-8" />
-	<meta name="description" content="" /> 
-	<meta name="robots" content="index, follow" /> 
-	
+	<meta name="description" content="" />
+	<meta name="robots" content="index, follow" />
+
 	<!-- stylesheets -->
 	<link rel="stylesheet" href="/assets/styles/css/screen.css" media="screen" />
-	
+
 	<!-- javascript -->
 	<!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,11 +1,11 @@
 // javascript that needs to run when the page is ready goes here.
 $(document).ready(function() {
-	
+
 	// smooth anchor
-	// add a class of scroll to any anchor link and this will smoothly scroll to the location. 
+	// add a class of scroll to any anchor link and this will smoothly scroll to the location.
 	$(".scroll").click(function(event){
 		event.preventDefault();
-		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+		$('html,body').animate({scrollTop:$(this.hash).offset().top}, 100);
 	});
 
 });
