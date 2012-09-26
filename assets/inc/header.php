ad1ead8c21c8f3b9eda35d78ad545289e914ac5d
Ben Lilley <benlilley@me.com>
2012-09-26 15:56:32 +1200

    Tweaked the header to provide better IE support and upgrade bar.

--- assets/inc/header.php
+++ assets/inc/header.php
@@ -10,6 +10,7 @@
 	<meta charset="utf-8" />
 	<meta name="description" content="" />
 	<meta name="robots" content="index, follow" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
 	<!-- stylesheets -->
 	<link rel="stylesheet" href="/assets/styles/css/screen.css" media="screen" />
@@ -18,3 +19,9 @@
 	<!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body>
+
+	<!-- old browser warning -->
+	<!--[if lt IE 8]>
+		<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or
+		<a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+	<![endif]-->
